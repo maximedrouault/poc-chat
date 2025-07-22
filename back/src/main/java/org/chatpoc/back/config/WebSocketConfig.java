787a2aee0e0cd1b5a337115becd79fe54ac4dc3f
maximedrouault <maximedrouault@gmail.com>
2025-07-22 15:01:21 +0200

    add WebSocket support with ChatHandler and WebSocketConfig

---
+++ back/src/main/java/org/chatpoc/back/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package org.chatpoc.back.config;
+
+import org.chatpoc.back.handler.ChatHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatHandler(), "/ws-chat").setAllowedOrigins("*");
+    }
+}
---
+++ back/src/main/java/org/chatpoc/back/handler/ChatHandler.java
@@ -0,0 +1,40 @@
+package org.chatpoc.back.handler;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatHandler extends TextWebSocketHandler {
+
+    private final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+        System.out.println("New session established: " + session.getId());
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println("Received message: " + message.getPayload() + " from session: " + session.getId());
+
+        synchronized (sessions) {
+            for (WebSocketSession s : sessions) {
+                if (s.isOpen()) {
+                    s.sendMessage(message);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+        System.out.println("Session closed: " + session.getId() + " with status: " + status);
+    }
+}
